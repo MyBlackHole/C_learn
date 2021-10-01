062eed48ea4b8070a091e0800059f6d28929f86b
BlackHole <1358244533@qq.com>
2021-10-01 11:31:47 +0800

    ctype

---
+++ ctype_learn/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+/* 返回值为非零（真）表示c是空白符，返回值为零（假）表示c不是空白符 */
+/*
+ *  空白符
+ *  ' ', '\t', '\v', '\f', '\r'
+ */
+
+void func1()
+{
+    char c;
+    int i=0;
+    char str[]="Example sentence to test isspace\n";
+    while (str[i])
+    {
+        c=str[i];
+        if (isspace(c)) c='\n';
+        putchar (c);
+        i++;
+    }
+}
+
+int main ()
+{
+    func1();
+    return 0;
+}
