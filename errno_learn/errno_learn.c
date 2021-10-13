0e20f51ab148c9fbad0f15bd12922a4442bd8377
BlackHole <1358244533@qq.com>
2021-10-13 12:43:46 +0800

    errno

---
+++ errno_learn/errno_learn.c
@@ -0,0 +1,13 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main (int argc,char ** argv)
+{
+	fprintf(stderr,"EACCESS :%s\n",strerror(EACCES));
+	errno=ENOENT;
+	perror(argv[0]);
+	exit(0);	
+}
