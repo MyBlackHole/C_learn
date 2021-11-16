f99d223d4130be27dd4c4ac2e3bbdf25d8ee29f2
BlackHole <1358244533@qq.com>
2021-11-16 18:04:44 +0800

    deleted

--- grammar/1.c
+++ grammar/1.c
@@ -22,116 +22,115 @@
  * 十进制、八进制、十六进制
  *
  */
-struct test1
-{
-    /* data */
-    int a : 2;
-    int b;
+struct test1 {
+  /* data */
+  int a : 2;
+  int b;
 } tt;
 
-
 int func9() {
-    struct test1 ttt;
-    ttt.b = 9;
-    printf("%p\n", &tt);
-    printf("%p\n", &tt.b);
-    unsigned long int count = (unsigned long int)&tt.b - (unsigned long int)&tt;
-    printf("%lu\n", count);
-    printf("%p\n", &ttt);
-    printf("%p\n", &ttt.b);
-    unsigned long int a = (unsigned long int)&ttt;
-    printf("%lu\n", a);
+  struct test1 ttt;
+  ttt.b = 9;
+  printf("%p\n", &tt);
+  printf("%p\n", &tt.b);
+  unsigned long int count = (unsigned long int)&tt.b - (unsigned long int)&tt;
+  printf("%lu\n", count);
+  printf("%p\n", &ttt);
+  printf("%p\n", &ttt.b);
+  unsigned long int a = (unsigned long int)&ttt;
+  printf("%lu\n", a);
 
-    ttt.a = 1;
+  ttt.a = 1;
 
-    // int 
-    printf("%d\n", *(int *)((unsigned long int)&ttt + count));
-    printf("%d\n", ttt.a);
-    return 0;
+  // int
+  printf("%d\n", *(int *)((unsigned long int)&ttt + count));
+  printf("%d\n", ttt.a);
+  return 0;
 }
-
 
 int func8() {
-    printf("%p\n", &tt);
-    printf("%p\n", &tt);
-    printf("%lu\n", sizeof(tt));
-    return 0;
+  printf("%p\n", &tt);
+  printf("%p\n", &tt);
+  printf("%lu\n", sizeof(tt));
+  return 0;
 }
 
-struct test
-{
-    /* data */
-    int a : 2;
+struct test {
+  /* data */
+  int a : 2;
 };
 
 int func7() {
-    struct test t;
-    printf("%d\n", t.a);
-    return 0;
+  struct test t;
+  printf("%d\n", t.a);
+  return 0;
 }
-
-
 
 int func6() {
-    int i = 1000;
-    printf("八进制:%d\n", (i));
-    return 0;
+  int i = 1000;
+  printf("八进制:%d\n", (i));
+  return 0;
 }
 
-
 int func5() {
-    int i = 1000;
-    printf("八进制:%o\n", i);
-    printf("十进制:%d\n", i);
-    printf("十六进制:%X\n", ~0);
-    return 0;
+  int i = 1000;
+  printf("八进制:%o\n", i);
+  printf("十进制:%d\n", i);
+  printf("十六进制:%X\n", ~0);
+  return 0;
 }
 
 int func4() {
-    int i = 0;
-    printf("%d", ~i);
-    return 0;
+  int i = 0;
+  printf("%d", ~i);
+  return 0;
 }
 
 int func3() {
-    int i = 0;
-    i &= 1 << 2;
-    printf("%d", i);
-    return 0;
+  int i = 0;
+  i &= 1 << 2;
+  printf("%d", i);
+  return 0;
 }
 
 int func2() {
-    int i = 0;
-    i |= 1 << 2;
-    printf("%d", i);
-    return 0;
+  int i = 0;
+  i |= 1 << 2;
+  printf("%d", i);
+  return 0;
 }
-
 
 int func1() {
-    printf("%d\n", 1 ? 1 : 2);
-    return 0;
+  printf("%d\n", 1 ? 1 : 2);
+  return 0;
 }
 
-
 void func10() {
-    if (1) {
-        printf("%d\n", 0);
-    }
-    return;
+  if (1) {
+    printf("%d\n", 0);
+  }
+  return;
 }
 
 void func11() {
-    int i = 1;
-    printf("%d", 1 + --i);
+  int i = 1;
+  printf("%d", 1 + --i);
 }
 
 void func12() {
-    int i = 1;
-    printf("%d", 1 + --i), printf("ok");
+  int i = 1;
+  printf("%d", 1 + --i), printf("ok");
 }
 
+/* # define EV_A */
+/* void func13() { */
+/*   if (!EV_A) { */
+/*     printf("触发了"); */
+/*   } */
+/*   printf("退出了"); */
+/* } */
+
 int main() {
-    func12();
-    exit(0);
+  func12();
+  exit(0);
 }
---
+++ time_learn/clock_gettime_test.c
@@ -0,0 +1,20 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+ 
+int main(int argc, char **argv)
+{
+    struct timespec time1 = {0, 0}; 
+    struct timespec time2 = {0, 0};
+	
+    float temp;
+ 
+    /* 获取时间 */
+    clock_gettime(CLOCK_REALTIME, &time1);      
+    /* 挂起程序 */
+    usleep(1000);
+    clock_gettime(CLOCK_REALTIME, &time2);   
+    temp = (time2.tv_nsec - time1.tv_nsec) / 1000000;
+    printf("time = %f ms\n", temp);
+    return 0;
+}
