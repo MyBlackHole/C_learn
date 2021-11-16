e03d4f7fbc5a7654369c494fdfbfdbbaed058828
BlackHole <1358244533@qq.com>
2021-11-16 09:21:05 +0800

    assert

---
+++ assert_learn/assert_test.c
@@ -0,0 +1,21 @@
+#include <signal.h>
+#include <stdio.h>
+
+/* 禁用assert */
+/* #define NDEBUG */
+
+#include <assert.h>
+
+
+static void handler(int signal) {
+    printf("触发了-[%d]\n", signal);
+}
+
+
+int main () {
+    signal(SIGABRT, handler);
+    /* signal(SIGABRT, SIG_IGN); */
+    assert(1);
+    assert(0);
+    printf("正常结束了");
+}
