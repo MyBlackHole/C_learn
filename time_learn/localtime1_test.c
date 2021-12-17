34139559d80f800b9aa047cdbb74a6f9fed7e02c
BlackHole <1358244533@qq.com>
2021-12-17 08:45:03 +0800

    time

---
+++ time_learn/localtime1_test.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define FNAME "/tmp/out"
+
+#define BUFSIZE 1024
+
+int main() {
+  FILE *fp = NULL;
+  char buf[BUFSIZE];
+  int count = 0;
+  time_t stamp;
+  struct tm *tm = NULL;
+
+  fp = fopen(FNAME, "a+");
+  if (fp == NULL) {
+    perror("fopen erron");
+    exit(1);
+  }
+
+  while (fgets(buf, BUFSIZE, fp) != NULL) {
+    count++;
+  }
+
+  while (1) {
+    time(&stamp);
+    tm = localtime(&stamp);
+    fprintf(fp, "%d----%d-%d-%d %d:%d:%d\n", ++count, tm->tm_year + 1900,
+            tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+    fflush(fp);
+    sleep(1);
+  }
+
+  fclose(fp);
+
+  exit(0);
+}
