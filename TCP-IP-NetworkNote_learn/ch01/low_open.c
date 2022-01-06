70211b76d1a2c9f732e3de9b4de792cd9cf5b2bf
BlackHole <1358244533@qq.com>
2022-01-06 23:56:58 +0800

    ch01

--- TCP-IP-NetworkNote_learn/ch01/low_open.c
+++ TCP-IP-NetworkNote_learn/ch01/low_open.c
@@ -1,28 +1,36 @@
+#include <fcntl.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <string.h>
 #include <unistd.h>
 void error_handling(char *message);
 
-int main()
-{
-    int fd;
-    char buf[] = "Let's go!\n";
-    // O_CREAT | O_WRONLY | O_TRUNC 是文件打开模式，将创建新文件，并且只能写。如存在 data.txt 文件，则清空文件中的全部数据。
-    fd = open("data.txt", O_CREAT | O_WRONLY | O_TRUNC);
-    if (fd == -1)
-        error_handling("open() error!");
-    printf("file descriptor: %d \n", fd);
-    // 向对应 fd 中保存的文件描述符的文件传输 buf 中保存的数据。
-    if (write(fd, buf, sizeof(buf)) == -1)
-        error_handling("write() error!");
-    close(fd);
-    return 0;
+int main(int argc, char *argv[]) {
+  int fd;
+  char *path = dirname(argv[0]);
+  char *filename = "data.txt";
+  char *name = malloc(strlen(path) + strlen(filename));
+  char buf[] = "Let's go!\n";
+  // O_CREAT | O_WRONLY | O_TRUNC
+  // 是文件打开模式，将创建新文件，并且只能写。如存在 data.txt
+  // 文件，则清空文件中的全部数据。
+
+  // S_IRUSR | S_IWUSR | S_IXUSR
+  // 文件访问权限
+  fd = open(name, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IXUSR);
+  if (fd == -1)
+    error_handling("open() error!");
+  printf("file descriptor: %d \n", fd);
+  // 向对应 fd 中保存的文件描述符的文件传输 buf 中保存的数据。
+  if (write(fd, buf, sizeof(buf)) == -1)
+    error_handling("write() error!");
+  close(fd);
+  return 0;
 }
 
-void error_handling(char *message)
-{
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
+void error_handling(char *message) {
+  fputs(message, stderr);
+  fputc('\n', stderr);
+  exit(1);
 }
--- TCP-IP-NetworkNote_learn/ch01/low_read.c
+++ TCP-IP-NetworkNote_learn/ch01/low_read.c
@@ -1,29 +1,32 @@
+#include <fcntl.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <string.h>
 #include <unistd.h>
 #define BUF_SIZE 100
 void error_handling(char *message);
 
-int main()
-{
-    int fd;
-    char buf[BUF_SIZE];
+int main(int argc, char *argv[]) {
+  char *path = dirname(argv[0]);
+  char *filename = "data.txt";
+  char *name = malloc(strlen(path) + strlen(filename));
+  int fd;
+  char buf[BUF_SIZE];
 
-    fd = open("data.txt", O_RDONLY);
-    if (fd == -1)
-        error_handling("open() error!");
-    printf("file descriptor: %d \n", fd);
+  fd = open(name, O_RDONLY);
+  if (fd == -1)
+    error_handling("open() error!");
+  printf("file descriptor: %d \n", fd);
 
-    if (read(fd, buf, sizeof(buf)) == -1)
-        error_handling("read() error!");
-    printf("file data: %s", buf);
-    close(fd);
-    return 0;
+  if (read(fd, buf, sizeof(buf)) == -1)
+    error_handling("read() error!");
+  printf("file data: %s", buf);
+  close(fd);
+  return 0;
 }
-void error_handling(char *message)
-{
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
+void error_handling(char *message) {
+  fputs(message, stderr);
+  fputc('\n', stderr);
+  exit(1);
 }
