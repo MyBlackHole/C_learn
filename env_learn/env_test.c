db9df16a8084ff0bad7d6a57282d047107f31076
black <1358244533@qq.com>
2022-01-10 14:55:10 +0800

    pthread_join

--- env_learn/env_test.c
+++ env_learn/env_test.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-int main(int argc,char **argv,char **env)
-{	
-	while(env&&*env)
-	{
-		printf("%s\n", *env);
-		++env;
-	}
-
+int main(int argc, char **argv, char **env) {
+  while (env && *env) {
+    printf("%s\n", *env);
+    ++env;
+  }
 }
