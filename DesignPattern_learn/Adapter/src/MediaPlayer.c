b596a7ca721b10899df8a17903d7da650683395f
black <1358244533@qq.com>
2022-01-11 10:52:14 +0800

    Adapter

--- DesignPattern_learn/Adapter/src/MediaPlayer.c
+++ DesignPattern_learn/Adapter/src/MediaPlayer.c
@@ -14,17 +14,20 @@
 #include "MediaPlayerStruct.h"
 #include "ctools.h"
 
-void media_player_destory(MediaPlayer** media_player) {
-    if (NULL == media_player || NULL == *media_player) return;
+void media_player_destory(MediaPlayer **media_player) {
+  if (NULL == media_player || NULL == *media_player)
+    return;
 
-    struct MediaPlayer* self = *media_player;
-    if (NULL != self->destroy) self->destroy(self);
-    free(*media_player);
-    *media_player = NULL;
+  struct MediaPlayer *self = *media_player;
+  if (NULL != self->destroy)
+    self->destroy(self);
+  free(*media_player);
+  *media_player = NULL;
 }
 
-void media_player_play(MediaPlayer* media_player, const char* audio_type, const char* file_name) {
-    if (NULL != media_player->play) {
-        media_player->play(media_player, audio_type, file_name);
-    }
+void media_player_play(MediaPlayer *media_player, const char *audio_type,
+                       const char *file_name) {
+  if (NULL != media_player->play) {
+    media_player->play(media_player, audio_type, file_name);
+  }
 }
