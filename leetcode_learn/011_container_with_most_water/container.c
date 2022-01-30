7981ba085dbcd97dcbc45e4de7c5af11aafbafae
black <1358244533@qq.com>
2022-01-30 22:16:07 +0800

    leetcode

--- leetcode_learn/011_container_with_most_water/container.c
+++ leetcode_learn/011_container_with_most_water/container.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int maxArea(int* height, int heightSize)
-{
-    int min = 0, max = heightSize - 1;
-    int area_max = 0;
-    while (min < max) {
-        int area = (max - min) * (height[min] < height[max] ? height[min] : height[max]);
-        area_max = area > area_max ? area : area_max;
-        if (height[min] < height[max]) {
-            min++;
-        } else {
-            max--;
-        }
+static int maxArea(int *height, int heightSize) {
+  int min = 0, max = heightSize - 1;
+  int area_max = 0;
+  while (min < max) {
+    int area =
+        (max - min) * (height[min] < height[max] ? height[min] : height[max]);
+    area_max = area > area_max ? area : area_max;
+    if (height[min] < height[max]) {
+      min++;
+    } else {
+      max--;
     }
-    return area_max;
+  }
+  return area_max;
 }
 
-int main(int argc, char **argv)
-{
-    int i, count = argc - 1;
-    int *nums = malloc(count * sizeof(int));
-    for (i = 0; i < count; i++) {
-        nums[i] = atoi(argv[i + 1]);
-    }
-    printf("%d\n", maxArea(nums, count));
-    return 0;
+int main(int argc, char **argv) {
+  int i, count = argc - 1;
+  int *nums = malloc(count * sizeof(int));
+  for (i = 0; i < count; i++) {
+    nums[i] = atoi(argv[i + 1]);
+  }
+  printf("%d\n", maxArea(nums, count));
+  return 0;
 }
--- leetcode_learn/013_roman_to_integer/roman_to_integer.cc
+++ leetcode_learn/013_roman_to_integer/roman_to_integer.cc
@@ -4,43 +4,43 @@ using namespace std;
 
 class Solution {
 public:
-    int romanToInt(string s) {
-        int curr = roman_to_integer(s[0]);
-        int result = curr;
+  int romanToInt(string s) {
+    int curr = roman_to_integer(s[0]);
+    int result = curr;
 
-        for (int i = 1; i < s.length(); i++) {
-            int prev = curr;
-            curr = roman_to_integer(s[i]);
-            /* left < right : IV(4), XL(40), IX(9) ... */
-            if (prev < curr) {
-                result -= prev - (curr - prev);
-            } else {
-                result += curr;
-            }
-        }
-
-        return result;
+    for (int i = 1; i < s.length(); i++) {
+      int prev = curr;
+      curr = roman_to_integer(s[i]);
+      /* left < right : IV(4), XL(40), IX(9) ... */
+      if (prev < curr) {
+        result -= prev - (curr - prev);
+      } else {
+        result += curr;
+      }
     }
+
+    return result;
+  }
 
 private:
-    int roman_to_integer(char c) {
-        switch(c) {
-        case 'I':
-            return 1;
-        case 'V':
-            return 5;
-        case 'X':
-            return 10;
-        case 'L':
-            return 50;
-        case 'C':
-            return 100;
-        case 'D':
-            return 500;
-        case 'M':
-            return 1000;
-        default:
-            return 0;
-        }
+  int roman_to_integer(char c) {
+    switch (c) {
+    case 'I':
+      return 1;
+    case 'V':
+      return 5;
+    case 'X':
+      return 10;
+    case 'L':
+      return 50;
+    case 'C':
+      return 100;
+    case 'D':
+      return 500;
+    case 'M':
+      return 1000;
+    default:
+      return 0;
     }
+  }
 };
