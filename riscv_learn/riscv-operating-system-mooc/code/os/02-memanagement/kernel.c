6fb2729fb92a1f796ec1da718fe9b69d5d32f505
black <1358244533@qq.com>
2022-02-23 16:59:12 +0800

    page_test

--- riscv_learn/riscv-operating-system-mooc/code/os/02-memanagement/kernel.c
+++ riscv_learn/riscv-operating-system-mooc/code/os/02-memanagement/kernel.c
@@ -6,14 +6,16 @@
  */
 extern void uart_init(void);
 extern void page_init(void);
+extern void page_test(void);
 
-void start_kernel(void)
-{
-	uart_init();
-	uart_puts("Hello, RVOS!\n");
+void start_kernel(void) {
+  uart_init();
+  uart_puts("Hello, RVOS!\n");
 
-	page_init();
+  page_init();
 
-	while (1) {}; // stop here!
+  page_test();
+
+  while (1) {
+  }; // stop here!
 }
-
