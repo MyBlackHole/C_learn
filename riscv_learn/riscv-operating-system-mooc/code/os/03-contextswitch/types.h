99f4a4e6030c4654581e1a61264198f87a3bd67b
black <1358244533@qq.com>
2022-02-23 18:10:31 +0800

    context

--- riscv_learn/riscv-operating-system-mooc/code/os/03-contextswitch/types.h
+++ riscv_learn/riscv-operating-system-mooc/code/os/03-contextswitch/types.h
@@ -3,12 +3,12 @@
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
-typedef unsigned int  uint32_t;
+typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 
 /*
  * RISCV32: register is 32bits width
- */ 
+ */
 typedef uint32_t reg_t;
 
 #endif /* __TYPES_H__ */
