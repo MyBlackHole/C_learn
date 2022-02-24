bf8b944f636684579d5dfe3d5e46443b5964261c
black <1358244533@qq.com>
2022-02-24 16:58:12 +0800

    multitask

--- riscv_learn/riscv-operating-system-mooc/code/os/04-multitask/sched.c
+++ riscv_learn/riscv-operating-system-mooc/code/os/04-multitask/sched.c
@@ -15,29 +15,24 @@ struct context ctx_tasks[MAX_TASKS];
 static int _top = 0;
 static int _current = -1;
 
-static void w_mscratch(reg_t x)
-{
-	asm volatile("csrw mscratch, %0" : : "r" (x));
+static void w_mscratch(reg_t x) {
+  asm volatile("csrw mscratch, %0" : : "r"(x));
 }
 
-void sched_init()
-{
-	w_mscratch(0);
-}
+void sched_init() { w_mscratch(0); }
 
 /*
  * implment a simple cycle FIFO schedular
  */
-void schedule()
-{
-	if (_top <= 0) {
-		panic("Num of task should be greater than zero!");
-		return;
-	}
+void schedule() {
+  if (_top <= 0) {
+    panic("Num of task should be greater than zero!");
+    return;
+  }
 
-	_current = (_current + 1) % _top;
-	struct context *next = &(ctx_tasks[_current]);
-	switch_to(next);
+  _current = (_current + 1) % _top;
+  struct context *next = &(ctx_tasks[_current]);
+  switch_to(next);
 }
 
 /*
@@ -48,34 +43,29 @@ void schedule()
  * 	0: success
  * 	-1: if error occured
  */
-int task_create(void (*start_routin)(void))
-{
-	if (_top < MAX_TASKS) {
-		ctx_tasks[_top].sp = (reg_t) &task_stack[_top][STACK_SIZE - 1];
-		ctx_tasks[_top].ra = (reg_t) start_routin;
-		_top++;
-		return 0;
-	} else {
-		return -1;
-	}
+int task_create(void (*start_routin)(void)) {
+  if (_top < MAX_TASKS) {
+    ctx_tasks[_top].sp = (reg_t)&task_stack[_top][STACK_SIZE - 1];
+    ctx_tasks[_top].ra = (reg_t)start_routin;
+    _top++;
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
 /*
  * DESCRIPTION
- * 	task_yield()  causes the calling task to relinquish the CPU and a new 
+ * 	task_yield()  causes the calling task to relinquish the CPU and a new
  * 	task gets to run.
  */
-void task_yield()
-{
-	schedule();
-}
+void task_yield() { schedule(); }
 
 /*
  * a very rough implementaion, just to consume the cpu
  */
-void task_delay(volatile int count)
-{
-	count *= 50000;
-	while (count--);
+void task_delay(volatile int count) {
+  count *= 50000;
+  while (count--)
+    ;
 }
-
