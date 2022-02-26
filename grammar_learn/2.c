41d657b9f1a3f9edeaeddba63d65409024089df3
black <1358244533@qq.com>
2022-02-26 22:36:26 +0800

    --

--- grammar_learn/2.c
+++ grammar_learn/2.c
@@ -1,10 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-int main(void)
-{
-    char *ptr="Linux";
-    printf("%c\n",*ptr++);
-    printf("%c\n",*ptr);
-    return 0;
+int main(void) {
+  char *ptr = "Linux";
+  printf("%c\n", *ptr++);
+  printf("%c\n", *ptr);
+  return 0;
 }
