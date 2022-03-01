bbd90c3f9e71425a04abb09d74d7494f3d3e043e
black <1358244533@qq.com>
2022-03-01 10:59:21 +0800

    strstr

---
+++ string_learn/strstr_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char *s = "GoldenGlobalView";
+  char *l = "lob";
+  char *p;
+  p = strstr(s, l);
+  if (p)
+    printf("%s\n", p);
+  else
+    printf("NotFound!\n");
+
+  return 0;
+}
