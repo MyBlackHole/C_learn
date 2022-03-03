d16cd1ab5917f2268ac81d055c3729b6ebe853c0
black <1358244533@qq.com>
2022-03-03 10:01:40 +0800

    lock

--- riscv_learn/riscv-operating-system-mooc/code/os/09-lock/lock.c
+++ riscv_learn/riscv-operating-system-mooc/code/os/09-lock/lock.c
@@ -1,13 +1,18 @@
 #include "os.h"
 
-int spin_lock()
-{
-	w_mstatus(r_mstatus() & ~MSTATUS_MIE);
-	return 0;
+static reg_t x;
+
+extern void amoswap_w_aq(reg_t *x);
+extern void amoswap_w_rl(reg_t *x);
+
+int spin_lock() {
+  amoswap_w_aq(&x);
+  // w_mstatus(r_mstatus() & ~MSTATUS_MIE);
+  return 0;
 }
 
-int spin_unlock()
-{
-	w_mstatus(r_mstatus() | MSTATUS_MIE);
-	return 0;
+int spin_unlock() {
+  amoswap_w_rl(&x);
+  // w_mstatus(r_mstatus() | MSTATUS_MIE);
+  return 0;
 }
--- riscv_learn/riscv-operating-system-mooc/code/os/09-lock/user.c
+++ riscv_learn/riscv-operating-system-mooc/code/os/09-lock/user.c
@@ -3,43 +3,50 @@
 #define DELAY 4000
 
 #define USE_LOCK
+static reg_t j;
 
-void user_task0(void)
-{
-	uart_puts("Task 0: Created!\n");
-	while (1) {
+void user_task0(void) {
+  uart_puts("Task 0: Created!\n");
+  while (1) {
+    uart_puts("Task 0: Begin ... \n");
+    j = 0;
 #ifdef USE_LOCK
-		spin_lock();
+    spin_lock();
 #endif
-		uart_puts("Task 0: Begin ... \n");
-		for (int i = 0; i < 5; i++) {
-			uart_puts("Task 0: Running... \n");
-			task_delay(DELAY);
-		}
-		uart_puts("Task 0: End ... \n");
+    for (int i = 0; i < 5; i++) {
+      printf("user_task0: %d\n", j++);
+      // uart_puts("Task 0: Running... \n");
+      task_delay(DELAY);
+    }
 #ifdef USE_LOCK
-		spin_unlock();
+    spin_unlock();
 #endif
-	}
+    uart_puts("Task 0: End ... \n");
+  }
 }
 
-void user_task1(void)
-{
-	uart_puts("Task 1: Created!\n");
-	while (1) {
-		uart_puts("Task 1: Begin ... \n");
-		for (int i = 0; i < 5; i++) {
-			uart_puts("Task 1: Running... \n");
-			task_delay(DELAY);
-		}
-		uart_puts("Task 1: End ... \n");
-	}
+void user_task1(void) {
+  uart_puts("Task 1: Created!\n");
+  while (1) {
+    uart_puts("Task 1: Begin ... \n");
+#ifdef USE_LOCK
+    spin_lock();
+#endif
+    j = 0;
+    for (int i = 0; i < 5; i++) {
+      // uart_puts("Task 1: Running... \n");
+      printf("user_task1: %d\n", j++);
+      task_delay(DELAY);
+    }
+#ifdef USE_LOCK
+    spin_unlock();
+#endif
+    uart_puts("Task 1: End ... \n");
+  }
 }
 
 /* NOTICE: DON'T LOOP INFINITELY IN main() */
-void os_main(void)
-{
-	task_create(user_task0);
-	task_create(user_task1);
+void os_main(void) {
+  task_create(user_task0);
+  task_create(user_task1);
 }
-
