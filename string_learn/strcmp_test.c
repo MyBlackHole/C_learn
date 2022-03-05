fdc154d32101ee75f04d02a197232e81c8cd7378
black <1358244533@qq.com>
2022-03-05 16:41:17 +0800

    strcmp

--- string_learn/strcmp_test.c
+++ string_learn/strcmp_test.c
@@ -5,9 +5,10 @@ int main(int argc, char *argv[]) {
   char str1[10];
   char str2[10];
   int ret;
-  strcpy(str1, "abc");
-  strcpy(str2, "abc");
+  strcpy(str1, "dbcbc");
+  strcpy(str2, "bcb");
   ret = strcmp(str1, str2);
+  printf("%d\n", ret);
   if (ret > 0) {
     printf("str1 > str2\n");
   } else if (ret < 0) {
