40b413b6ba5a023477a6f78bf080020ab2133000
black <1358244533@qq.com>
2022-03-08 14:36:54 +0800

    redis

--- redis_learn/Redis-Code/baseinfo/version.h
+++
@@ -1,2 +0,0 @@
-/* 就宏定义了一个版本号 */
-#define REDIS_VERSION "2.8.17"
---
+++ seccomp_learn/PR_SET_NAME_test.c
@@ -0,0 +1,15 @@
+/*
+gcc changetitle.c -o changetitle
+*/
+#include <stdio.h>
+#include <sys/prctl.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+  char *new_name = "littlehann-program";
+
+  getchar();
+  prctl(PR_SET_NAME, new_name);
+  getchar();
+
+  return 0;
+}
