33ab214c45f8634c657296f0bb6f466dfb80b83a
black <1358244533@qq.com>
2022-03-10 14:10:22 +0800

    SIGABRT

--- assert_learn/assert_test.c
+++ assert_learn/assert_test.c
@@ -10,7 +10,7 @@ static void handler(int signal) { printf("触发了-[%d]\n", signal); }
 int main() {
   signal(SIGABRT, handler);
   /* signal(SIGABRT, SIG_IGN); */
-  assert(1);
-  // assert(0);
+  // assert(1);
+  assert(0);
   printf("正常结束了");
 }
