b06c911e1ffd7fd0af0d1a5f8ac687ce36278e69
black <1358244533@qq.com>
2022-03-17 13:55:59 +0800

    BLKGETSIZE64

---
+++ ioctl_learn/ioctl_test.c
@@ -0,0 +1,42 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/fs.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void) {
+  int fd;
+  // off_t size
+  uint64_t size;
+  int len;
+  int r;
+
+  if ((fd = open("/dev/sda", O_RDONLY)) < 0) {
+    printf("open error %d\n", errno);
+    exit(-1);
+  }
+
+#if 0
+    //会报错
+    if ((size = lseek(fd, 0, SEEK_END)) < 0)
+    {
+        printf("lseek errno %d\n", errno);
+        exit(-1);
+    }
+#endif
+  // 获取设备块数量
+  if ((r = ioctl(fd, BLKGETSIZE64, &size)) < 0) {
+    printf("ioctl error %d\n", errno);
+    exit(-1);
+  }
+
+  len = (size >> 30);
+  printf("size of sda = %d G\n", len);
+
+  return 0;
+}
