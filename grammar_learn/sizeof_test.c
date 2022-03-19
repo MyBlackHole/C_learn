0d6bace59c073118a787bdf86abfa59617ae69c9
black <1358244533@qq.com>
2022-03-19 19:08:16 +0800

    SHA1

--- grammar_learn/sizeof_test.c
+++ grammar_learn/sizeof_test.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[]) {
   int *p2 = malloc(1000);
   // 无法获取 malloc 空间大小
-  printf("%ld\n", sizeof(*p2));
+  printf("*p2-%ld\n", sizeof(*p2));
+  printf("p2-%ld\n", sizeof(p2));
   exit(0);
 }
---
+++ grammar_learn/struct1_test.c
@@ -0,0 +1,20 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Line {
+  uint32_t length;
+  // 不占用空间, 柔性数组用法
+  uint8_t contents[];
+};
+
+int main() {
+  struct Line l;
+  printf("%ld\n", sizeof(l));
+
+  int length = 100;
+  struct Line *lp = (struct Line *)malloc(sizeof(struct Line) + length);
+  // 代表 contents 100 char
+  lp->length = 100;
+  return 0;
+}
