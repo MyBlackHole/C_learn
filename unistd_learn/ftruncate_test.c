085acde0c28fa5c4c423360af1d2e1c2563a4101
black <1358244533@qq.com>
2022-03-21 16:05:21 +0800

    ftruncate

---
+++ unistd_learn/ftruncate_test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// 文件截取
+int main(int argc, char *argv[]) {
+  char *filename = argv[1];
+  int length = atoi(argv[2]);
+  // length 超过文件原长度零填充，小于文件原长度截断保留 length
+  // 文件最终长度都是 length
+  truncate(filename, length);
+  return 0;
+}
