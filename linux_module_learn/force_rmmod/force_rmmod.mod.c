678b876ccb923be12c33ac44c353c021a1dffcef
black <1358244533@qq.com>
2023-11-20 22:35:59 +0800

    linux force_rmmod test

---
+++ linux_module_learn/force_rmmod/force_rmmod.mod.c
@@ -0,0 +1,61 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
+#include <linux/export-internal.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+BUILD_LTO_INFO;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+
+static const char ____versions[]
+__used __section("__versions") =
+	"\x10\x00\x00\x00\x7e\x3a\x2c\x12"
+	"_printk\0"
+	"\x1c\x00\x00\x00\xca\x39\x82\x5b"
+	"__x86_return_thunk\0\0"
+	"\x14\x00\x00\x00\xbb\x6d\xfb\xbd"
+	"__fentry__\0\0"
+	"\x10\x00\x00\x00\x5a\x25\xd5\xe2"
+	"strcmp\0\0"
+	"\x18\x00\x00\x00\xc6\x35\xc9\xc4"
+	"module_refcount\0"
+	"\x14\x00\x00\x00\xd5\xe3\x7d\x01"
+	"nr_cpu_ids\0\0"
+	"\x1c\x00\x00\x00\x53\x54\x9a\xb1"
+	"__per_cpu_offset\0\0\0\0"
+	"\x1c\x00\x00\x00\x75\x3f\x68\x9e"
+	"__cpu_possible_mask\0"
+	"\x18\x00\x00\x00\xd9\xe8\xa1\x53"
+	"_find_next_bit\0\0"
+	"\x28\x00\x00\x00\xb3\x1c\xa2\x87"
+	"__ubsan_handle_out_of_bounds\0\0\0\0"
+	"\x18\x00\x00\x00\x93\x87\x1e\xa7"
+	"param_ops_charp\0"
+	"\x18\x00\x00\x00\x6a\xb7\x61\x9e"
+	"module_layout\0\0\0"
+	"\x00\x00\x00\x00\x00\x00\x00\x00";
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "CB0E9716F1B90C5B125CAAB");
---
+++ linux_module_learn/force_rmmod/run.sh
@@ -0,0 +1,17 @@
+make
+
+#  通过 `modname` 制定待卸载驱动的信息
+sudo insmod force_rmmod.ko modname=hello
+#  查看是否加载成功, `exit` 函数是否正常替换
+dmesg | tail -l
+#  卸载 `hello` 驱动
+sudo rmmod hello
+#  卸载 `force_rmmod` 驱动
+sudo rmmod force_rmmod
+
+
+# [10042.899623] [before] name:hello, state:1, refcnt:1
+# [10042.899631] No modules depond on hello...
+# [10042.899633] [after] name:hello, state:0, refcnt:0
+# [10059.020356] hello_exit: exit called
+# [10065.500156] =======name : force_rmmod, state : 2 EXIT=======
