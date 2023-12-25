50ccde95dd131a14f7092449da26bec562a76b53
black <1358244533@qq.com>
2023-12-25 23:05:38 +0800

    shm 测试使用

--- sys_mman_learn/mmap_test.c
+++ sys_mman_learn/mmap_test.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,20 +8,24 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define NUM 10
+
 typedef struct
 {
     char name[4];
     int age;
 } people;
 
-int main(int argc, char **argv)  // map a normal file as shared mem:
+// map a normal file as shared mem:
+int demo_mmap_main(int argc, char **argv)
 {
-    int fd, i;
+    int myfd;
+    int index;
     people *p_map;
-    int rv;
+    size_t ret;
     // 以读写方式打开， 没有自动创建
-    fd = open(argv[1], O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
-    if (fd < 0)
+    myfd = open(argv[1], O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+    if (myfd < 0)
     {
         perror(strerror(errno));
         exit(EXIT_FAILURE);
@@ -28,30 +33,31 @@ int main(int argc, char **argv)  // map a normal file as shared mem:
 
     // 文件大小不能为 0
     // 否则读取异常
-    rv = write(fd, "\n", 1);
-    if (rv < 0)
+    ret = write(myfd, "\n", 1);
+    if (ret < 0)
     {
-        printf("write %s \n", strerror(rv));
+        printf("write %s \n", strerror((int)ret));
         exit(EXIT_FAILURE);
     }
 
-    p_map = mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE, MAP_SHARED,
-                 fd, 0);
+    p_map = mmap(NULL, sizeof(people) * NUM, PROT_READ | PROT_WRITE, MAP_SHARED,
+                 myfd, 0);
     if (p_map == MAP_FAILED)
     {
         perror(strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    char c[2];
-    for (i = 0; i < 10; i++)
+    char str[2];
+    for (index = 0; index < NUM; index++)
     {
-        (*(p_map + i)).age = i;
-        sprintf(c, "%d", i);
-        memccpy(&(*(p_map + i)).name, &c, sizeof(char), 2);
-        printf("name: %s age %d;\n", (*(p_map + i)).name, (*(p_map + i)).age);
+        (*(p_map + index)).age = index;
+        sprintf(str, "%d", index);
+        memccpy(&(*(p_map + index)).name, &str, sizeof(char), 2);
+        printf("name: %s age %d;\n", (*(p_map + index)).name,
+               (*(p_map + index)).age);
     }
     // 解除映射时会把内存数据写回文件，但是不会超出文件大小
-    munmap(p_map, sizeof(people) * 10);
+    munmap(p_map, sizeof(people) * NUM);
     exit(EXIT_SUCCESS);
 }
---
+++ sys_mman_learn/sys_mman_learn.h
@@ -0,0 +1,11 @@
+#ifndef AIO_LEARN_H
+#define AIO_LEARN_H
+
+#include "../lib/demo.h"
+DEMO_MAIN_DECL(mmap);
+DEMO_MAIN_DECL(shm_writer);
+DEMO_MAIN_DECL(shm_reader);
+DEMO_MAIN_DECL(open_shm);
+DEMO_MAIN_DECL(shm_open);
+
+#endif
