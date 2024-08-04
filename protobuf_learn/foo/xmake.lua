49ac4668281886c33c107a3a840e9d26eddcdada
black <1358244533@qq.com>
2024-08-04 19:03:54 +0800

    protobuf socket 测试

--- protobuf_learn/foo/xmake.lua
+++ protobuf_learn/foo/xmake.lua
@@ -1,14 +1,11 @@
-local dir_path = path.relative(os.curdir(), os.projectdir())
-
 -- 使用非系统的protobuf-c库，并设置为静态库
 add_requires("protobuf-c", {system = false, configs = {static = true}, debug = is_mode("debug")})
 -- add_requires("protobuf-c")
 
-target(dir_path)
+target("protobuf_learn_foo")
     set_kind("binary")
     add_packages("protobuf-c")
     add_rules("protobuf.c")
     add_files("*.c")
-    add_files("*.proto", {proto_rootdir = dir_path})
-    add_files("subdir/*.proto", {proto_rootdir = dir_path})
+    add_files("*.proto", {proto_rootdir = "protobuf_learn/foo"})
 
