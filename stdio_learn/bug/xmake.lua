4bfab29b9e3660ec4161759d80666cc70d889115
black <1358244533@qq.com>
2024-08-10 18:57:34 +0800

    [stdio] popen 测试 fclose pclose 是否一致

---
+++ stdio_learn/bug/xmake.lua
@@ -0,0 +1,11 @@
+target("stdio_learn_buf_print")
+    set_kind("binary")
+    add_files("print_test.c")
+
+target("stdio_learn_buf_fgets")
+    set_kind("binary")
+    add_files("fgets_test.c")
+
+target("stdio_learn_buf_popen")
+    set_kind("binary")
+    add_files("popen_test.c")
