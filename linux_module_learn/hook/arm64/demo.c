218e97f4226027b3248c50880795786f018a1123
black <1358244533@qq.com>
2024-08-16 14:17:24 +0800

    [linux][module][arm64] 补充不同的修改只读地址方式，用于修改 hook 地址

--- linux_module_learn/hook/arm64/demo.c
+++ linux_module_learn/hook/arm64/demo.c
@@ -9,28 +9,36 @@
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
 
+static unsigned long *__sys_call_table;
+
+#define UPDATE_MAPPING_PROT 0
+
+#if UPDATE_MAPPING_PROT
+void (*update_mapping_prot)(phys_addr_t phys, unsigned long virt,
+			    phys_addr_t size, pgprot_t prot);
+unsigned long start_rodata, end_rodata;
+#define section_size (end_rodata - start_rodata)
+static void disable_wirte_protection(void)
+{
+	update_mapping_prot(__pa_symbol(start_rodata),
+			    (unsigned long)start_rodata, section_size,
+			    PAGE_KERNEL);
+	return;
+}
+
+static void enable_wirte_protection(void)
+{
+	update_mapping_prot(__pa_symbol(start_rodata),
+			    (unsigned long)start_rodata, section_size,
+			    PAGE_KERNEL_RO);
+	return;
+}
+
+#else
+
+static unsigned long mkdir_sys_call_addr;
 static pte_t *ptep;
 static struct mm_struct *mm;
-
-static unsigned long *__sys_call_table;
-static unsigned long mkdir_sys_call_addr;
-
-typedef long (*syscall_fn_t)(const struct pt_regs *regs);
-
-#ifndef __NR_mkdirat
-#define __NR_mkdirat 34
-#endif
-
-//用于保存原始的 mkdir 系统调用
-static syscall_fn_t orig_mkdir;
-
-asmlinkage long mkdir_hook(const struct pt_regs *regs)
-{
-	printk("hook mkdir sys_call\n");
-
-	//return orig_mkdir(regs);
-	return 0;
-}
 
 static void set_pte_write(void)
 {
@@ -65,21 +73,51 @@ static void set_pte_rdonly(void)
 	__flush_tlb_kernel_pgtable(mkdir_sys_call_addr);
 }
 
+#endif
+
+//用于保存原始的 mkdir 系统调用
+typedef long (*syscall_fn_t)(const struct pt_regs *regs);
+static syscall_fn_t orig_mkdir;
+
+asmlinkage long mkdir_hook(const struct pt_regs *regs)
+{
+	printk("hook mkdir sys_call\n");
+
+	//return orig_mkdir(regs);
+	return 0;
+}
+
 //内核模块初始化函数
 static int __init lkm_init(void)
 {
+#ifndef UPDATE_MAPPING_PROT
 	pgd_t *pgdp;
 	pud_t *pudp;
 	pmd_t *pmdp;
-
-	/* can be directly found in kernel memory */
-	mm = (struct mm_struct *)kallsyms_lookup_name("init_mm");
-	if (mm == NULL)
-		return -1;
+#endif
 
 	__sys_call_table =
 		(unsigned long *)kallsyms_lookup_name("sys_call_table");
 	if (!__sys_call_table)
+		return -1;
+#if UPDATE_MAPPING_PROT
+
+	// 获取只读页表的更新函数
+	update_mapping_prot =
+		(void *)kallsyms_lookup_name("update_mapping_prot");
+	// 获取只读页表的起始地址和结束地址
+	start_rodata = (unsigned long)kallsyms_lookup_name("__start_rodata");
+	end_rodata = (unsigned long)kallsyms_lookup_name("__end_rodata");
+	LOG_INFO("update_mapping_prot:%lx, start_rodata:%lx, end_rodata:%lx.\n",
+		 update_mapping_prot, start_rodata, end_rodata);
+	disable_wirte_protection();
+	__sys_call_table[__NR_mkdirat] = (unsigned long)mkdir_hook;
+	enable_wirte_protection();
+#else
+
+	/* can be directly found in kernel memory */
+	mm = (struct mm_struct *)kallsyms_lookup_name("init_mm");
+	if (mm == NULL)
 		return -1;
 
 	mkdir_sys_call_addr = (unsigned long)(__sys_call_table + __NR_mkdirat);
@@ -114,6 +152,7 @@ static int __init lkm_init(void)
 	set_pte_write();
 	__sys_call_table[__NR_mkdirat] = (unsigned long)mkdir_hook;
 	set_pte_rdonly();
+#endif
 
 	printk("lkm_init\n");
 
@@ -123,9 +162,15 @@ static int __init lkm_init(void)
 //内核模块退出函数
 static void __exit lkm_exit(void)
 {
+#if UPDATE_MAPPING_PROT
 	set_pte_write();
 	__sys_call_table[__NR_mkdirat] = (unsigned long)orig_mkdir;
 	set_pte_rdonly();
+#else
+	disable_wirte_protection();
+	__sys_call_table[__NR_mkdirat] = (unsigned long)orig_mkdir;
+	enable_wirte_protection();
+#endif
 
 	printk("lkm_exit\n");
 }
