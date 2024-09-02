c5d855f9844da9638d7bb18f9adae6a262a630b1
black <1358244533@qq.com>
2024-09-02 16:10:01 +0800

    [string] strstr 配置测试，开头、中间

--- string_learn/strstr_test.c
+++ string_learn/strstr_test.c
@@ -1,17 +1,33 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-int demo_strstr_test_main()
+int demo_strstr_test_main(int argc, char **argv)
 {
 	char *str = "GoldenGlobalView";
-	char *str1 = "lob";
 	char *str2;
-	str2 = strstr(str, str1);
+
+	if (argc < 2) {
+		printf("Usage: %s substring\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	str2 = strstr(str, argv[1]);
 	if (str2) {
 		printf("%s\n", str2);
+		if (str2 == str) {
+			printf("Found at the beginning of the string!\n");
+		} else {
+			printf("Found at position %ld!\n", str2 - str);
+		}
 	} else {
 		printf("NotFound!\n");
 	}
 
 	return 0;
 }
+
+// xmake run string_learn strstr_test lob
+// Output:
+// lobalView
+// Found at position 7!
