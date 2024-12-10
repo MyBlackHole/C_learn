a336ab09a3817ec822f7ab0c511eaf874137f0a4
black <1358244533@qq.com>
2024-12-11 07:23:35 +0800

    [module] 测试获取其他内核模块的物理地址是否位置一致

--- linux_module_learn/mm/phy_mm/phy_mm.c
+++ linux_module_learn/mm/phy_mm/phy_mm.c
@@ -13,7 +13,7 @@ static int __init my_init(void)
 	volatile u32 *va_data;
 
 	pr_info("Module init\n");
-	va_addr = ioremap(0x1e5a32048, 1024);
+	va_addr = ioremap(0x1e5ca5608, 1024);
 
 	if (va_addr == NULL) {
 		pr_err("Failed to ioremap\n");
@@ -38,3 +38,18 @@ module_init(my_init);
 module_exit(my_exit);
 
 MODULE_LICENSE("GPL v2");
+
+// output:
+//
+// # insmod hw_breakpoint.ko
+// [  300.166282] hw_breakpoint hello world!
+// [  300.168295] ptr = 0000000077b3e6e9
+// [  300.170003] ptr = 0xffff8881e5ca5608
+// [  300.172099] physical address = 0x1e5ca5608
+// [  300.174300] physical address = 0x1e5ca5608
+// [  300.177127] register_wide_hw_breakpoint success
+// # insmod phy_mm.ko
+// [  336.737332] Module init
+// [  336.739025] VA: 0x00000000fdfb87db
+// [  336.740912] Old Data: 0x44332211
+// [  336.743147] New Data: 0x99999999
